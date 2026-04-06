2d137a536a7679f77636c48d8e79ba24b0ab4b76
leetingo <findjohntg@gmail.com>
2026-04-06 18:13:32 +0800

    fix nvim config for Neovim 0.12.0 compatibility
    - Replace removed nvim-treesitter.configs API with nvim-treesitter.setup
    - Separate nvim-treesitter-textobjects into independent plugin spec
    - Replace deprecated vim.loop with vim.uv
    - Remove vim-illuminate dependency (incompatible with new treesitter)
    - Add markdown FileType autocmd for vim regex highlighting
    - Update plugin lock file

--- .config/nvim/lua/tingo/lazy/treesitter.lua
+++ .config/nvim/lua/tingo/lazy/treesitter.lua
@@ -1,62 +1,50 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-        {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-            event = "VeryLazy"
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = {
+            "windwp/nvim-ts-autotag",
+            "nvim-treesitter/nvim-treesitter-context",
         },
-        "windwp/nvim-ts-autotag",
-        "nvim-treesitter/nvim-treesitter-context",
-        "RRethy/vim-illuminate",
+        build = ":TSUpdate",
+        config = function()
+            require('nvim-treesitter').setup({
+                ensure_installed = { "vim", "vimdoc", "lua" },
+                auto_install = true,
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "markdown",
+                callback = function()
+                    vim.bo.syntax = "on"
+                end,
+            })
+
+            require('nvim-ts-autotag').setup()
+            require("treesitter-context").setup({
+                max_lines = 1,
+                multiline_threshold = 1,
+                trim_scope = "outer",
+                separator = nil,
+            })
+        end,
     },
-    build = ":TSUpdate",
-    config = function()
-        require('nvim-treesitter.configs').setup({
-            -- A list of parser names
-            ensure_installed = { "vim", "vimdoc", "lua" },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" },
-            },
-
-            textobjects = {
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        event = "VeryLazy",
+        config = function()
+            require('nvim-treesitter-textobjects').setup({
                 select = {
                     enable = true,
-
-                    lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-
+                    lookahead = true,
                     keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
                         ["af"] = "@function.outer",
                         ["if"] = "@function.inner",
                         ["ac"] = "@class.outer",
                         ["ic"] = "@class.inner",
                     },
-                }
-            }
-        })
-        require('nvim-ts-autotag').setup()
-        require("treesitter-context").setup({
-          max_lines = 1,
-          multiline_threshold = 1,
-          trim_scope = "outer",
-          separator = nil,
-          -- patterns = { default = { "function", "method" } },
-        })
-    end
+                },
+            })
+        end,
+    },
 }
